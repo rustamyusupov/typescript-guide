14f2af88da975deec32dc0dec73454768b531f60
Rustam Yusupov <rustam.yusupov@gmail.com>
2021-09-30 10:20:58 +0200

    feat(index, company): define company

--- maps/src/Company.ts
+++ maps/src/Company.ts
@@ -1,14 +1,16 @@
 import faker from 'faker';
 
-export class User {
+export class Company {
   name: String;
+  catchPhrase: String;
   location: {
     lat: Number;
     lng: Number;
   };
 
   constructor() {
-    this.name = faker.name.firstName();
+    this.name = faker.company.companyName();
+    this.catchPhrase = faker.company.catchPhrase();
     this.location = {
       lat: Number(faker.address.latitude()),
       lng: Number(faker.address.longitude()),
