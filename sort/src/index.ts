15a13ce796ee25c7e7d0828358e9fe233be85ed8
Rustam Yusupov <rustam.yusupov@gmail.com>
2021-10-23 13:23:45 +0200

    feat(sort): sort characters and linkedlist collections

--- sort/src/index.ts
+++ sort/src/index.ts
@@ -1,23 +1,19 @@
-class Sorter {
-  constructor(public collection: number[]) {}
+import { CharactersCollection } from './CharactersCollection';
+import { LinkedList } from './LinkedList';
+import { NumbersCollection } from './NumbersCollection';
 
-  sort(): void {
-    const { length } = this.collection;
+const numbersCollection = new NumbersCollection([100, 3, -5, 0]);
+numbersCollection.sort();
+console.log('numbers', numbersCollection.data);
 
-    for (let i = 0; i < length; i++) {
-      for (let j = 0; j < length - i - 1; j++) {
-        if (this.collection[j] > this.collection[j + 1]) {
-          const leftHand = this.collection[j];
+const characterCollection = new CharactersCollection('aXbay');
+characterCollection.sort();
+console.log('numbers', characterCollection.data);
 
-          this.collection[j] = this.collection[j + 1];
-          this.collection[j + 1] = leftHand;
-        }
-      }
-    }
-  }
-}
-
-const sorter = new Sorter([10, 3, -5, 0]);
-
-sorter.sort();
-console.log(sorter.collection);
+const linkedList = new LinkedList();
+linkedList.add(100);
+linkedList.add(3);
+linkedList.add(-5);
+linkedList.add(0);
+linkedList.sort();
+linkedList.print();
