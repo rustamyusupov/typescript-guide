064ae18251debd41cd5e9cb6cd4a19b138ff3946
Rustam Yusupov <rustam.yusupov@gmail.com>
2021-10-24 13:59:09 +0200

    feat(index): add not good solution

--- stats/src/index.ts
+++ stats/src/index.ts
@@ -1,1 +1,17 @@
-console.log('hi there!');
+import fs from 'fs';
+
+const matches = fs
+  .readFileSync('football.csv', { encoding: 'utf-8' })
+  .split('\n')
+  .map((row: string): string[] => row.split(','));
+
+const manUnitedWins = matches.reduce(
+  (acc: number, cur: string[]): number =>
+    (cur[1] === 'Man United' && cur[5] === 'H') ||
+    (cur[2] === 'Man United' && cur[5] === 'A')
+      ? (acc += 1)
+      : acc,
+  0
+);
+
+console.log(`Man United won ${manUnitedWins} games`);
