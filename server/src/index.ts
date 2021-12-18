2f31a25c33bf51da34b2b49bb2c67da1eebc02f6
Rustam Yusupov <rustam.yusupov@gmail.com>
2021-12-18 14:16:55 +0200

    feat(server): add routes

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,16 @@
-console.log('object');
+import express, { Request, Response } from 'express';
+import bodyParser from 'body-parser';
+import cookieSession from 'cookie-session';
+
+import { router } from './routes/login';
+
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieSession({ keys: ['aso;ifjweqmr239'] }));
+app.use(router);
+
+app.listen(port, () => {
+  console.log(`Listening on http://localhost:${port}`);
+});
---
+++ server/src/routes/login.ts
@@ -0,0 +1,75 @@
+import { Router, Request, Response, NextFunction } from 'express';
+
+interface RequestWithBody extends Request {
+  body: {
+    [key: string]: string | undefined;
+  };
+}
+
+const router = Router();
+
+const requireAuth = (req: Request, res: Response, next: NextFunction): void => {
+  if (req.session && req.session.loggedIn) {
+    next();
+    return;
+  }
+
+  res.status(403);
+  res.send('Not permitted');
+};
+
+router.get('/', (req: Request, res: Response) => {
+  if (req.session && req.session.loggedIn) {
+    res.send(`
+      <div>
+        <p>You are logged in</p>
+        <a href="/logout">Logout</a>
+      </div>
+    `);
+  }
+
+  res.send(`
+    <div>
+      <p>You are not logged in</p>
+      <a href="/login">Login</a>
+    </div>
+  `);
+});
+
+router.get('/login', (req: Request, res: Response) => {
+  res.send(`
+    <form method="POST">
+      <div>
+        <label>Email</label>
+        <input name="email" />
+      </div>
+      <div>
+        <label>Password</label>
+        <input name="password" type="password" />
+      </div>
+      <button>Submit</button>
+    </form>
+  `);
+});
+
+router.post('/login', (req: RequestWithBody, res: Response) => {
+  const { email, password } = req.body;
+
+  if (email && password && email === 'i@rstm.me' && password === 'pass') {
+    req.session = { loggedIn: true };
+    res.redirect('/');
+  }
+
+  res.send('Invalid email or password');
+});
+
+router.get('/logout', (req: Request, res: Response) => {
+  req.session = undefined;
+  res.redirect('/');
+});
+
+router.get('/protected', requireAuth, (req: Request, res: Response) => {
+  res.send('Welcome to protected route');
+});
+
+export { router };
